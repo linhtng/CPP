8489934c53ae9b99e297da4156dc491af15f3d8e
linhtng <linh.nguyen361991@gmail.com>
2023-09-30 20:52:40 +0300

    start ex02, recreate Acccount.cpp

---
+++ CppModule00/ex02/Account.cpp
@@ -0,0 +1,50 @@
+#include "Account.hpp"
+
+/* PUBLIC METHODS */
+
+Account::Account(int initial_deposit) {}
+
+Account::~Account()
+{
+}
+
+int Account::getNbAccounts()
+{
+}
+
+int Account::getTotalAmount()
+{
+}
+
+int Account::getNbDeposits()
+{
+}
+
+int Account::getNbWithdrawals()
+{
+}
+void Account::displayAccountsInfos()
+{
+}
+
+void Account::makeDeposit(int deposit)
+{
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+}
+
+int Account::checkAmount() const
+{
+}
+
+void Account::displayStatus(void) const
+{
+}
+
+/* PRIVATE METHODS */
+
+void Account::_displayTimestamp()
+{
+}
