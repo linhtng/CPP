0cdc8c496a7cc7faa00183bfbb07f8b138f6216f
linhtng <linh.nguyen361991@gmail.com>
2023-10-16 22:18:43 +0300

    start cpp02/ex02

---
+++ cppModule02/ex02/Fixed.hpp
@@ -0,0 +1,30 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+public:
+    Fixed();
+    Fixed(int const integer);
+    Fixed(float const floatNumber);
+    Fixed(const Fixed &other);
+    Fixed &operator=(const Fixed &rhs);
+    Fixed operator+(Fixed const &rhs) const;
+    ~Fixed();
+
+    int getRawBits() const;
+    void setRawBits(int const raw);
+    float toFloat() const;
+    int toInt() const;
+
+private:
+    int rawBits;
+    static const int fractionalBits = 8;
+};
+
+std::ostream &operator<<(std::ostream &o, Fixed const &rhs);
+
+#endif
